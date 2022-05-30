d91f7a1340d4f90ba69e23a0e5b7215051a1ffc1
Kelvin Bush <kelybush@gmail.com>
2022-05-30 11:10:46 +0000

    add sass

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import styles from "./TodoItem.module.scss"
 
 class TodoItem extends React.Component {
   render() {
-    return <li>
+    return <li className={styles.item}>
       <input
         type="checkbox"
+        className={styles.checkbox}
         checked={this.props.todo.completed}
-        onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
-      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+        onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+      />
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+      </button>
       {this.props.todo.title}
-    </li>;
+    </li>
   }
 }
 
