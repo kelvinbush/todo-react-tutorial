98685a551853dfbb0741c74f78b93e0323155913
Kelvin Bush <kelybush@gmail.com>
2022-05-30 11:06:17 +0000

    add styling with css

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
@@ -63,13 +63,17 @@ class TodoContainer extends Component {
 
   render() {
     return (
-      <>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList todos={this.state.todos}
-                   handleChangeProps={this.handleChange}
-                   deleteTodoProps={this.delTodo} />
-      </>
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
+      </div>
     )
   }
 }
