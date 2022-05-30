7d367e686119f4cb3ffd7410d93a4eb6e0a73864
Kelvin Bush <kelybush@gmail.com>
2022-05-30 14:47:52 +0000

    use prettier

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const links = [
     {
       id: 1,
-      path: '/',
-      text: 'Home',
+      path: "/",
+      text: "Home",
     },
     {
       id: 2,
-      path: '/about',
-      text: 'About',
+      path: "/about",
+      text: "About",
     },
   ];
 
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+
   return (
     <nav className="navBar">
+      <button type="button" onClick={handleToggle}>
+        {navbarOpen ? "Close" : "Open"}
+      </button>
       <ul>
         {links.map((link) => (
           <li key={link.id}>
