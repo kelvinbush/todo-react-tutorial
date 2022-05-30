f47639fe13d461b29d47c9914308cad0fc7d97eb
Kelvin Bush <kelybush@gmail.com>
2022-05-30 12:08:48 +0000

    add routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import TodoContainer from "./functionBased/components/TodoContainer";
 import "./functionBased/App.css"
@@ -7,6 +8,8 @@ import "./functionBased/App.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <TodoContainer />
+    </Router>
   </React.StrictMode>
 );
