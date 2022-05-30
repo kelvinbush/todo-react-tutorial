a81685e21a471eadda7cf73b0837cd3170a536cf
Kelvin Bush <kelybush@gmail.com>
2022-05-30 10:28:02 +0000

    convert to arrow function

---
+++ src/components/TodoItem.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TodoItem = props => <li>{props.todo.title}</li>;
+
+export default TodoItem;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,15 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
+import TodoItem from "./TodoItem";
 
-class TodosList extends Component {
-  render() {
-    return (
-      <ul>
-        {this.props.todos.map(todo => (
-          <TodoItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = props => (
+  <ul>
+    {props.todos.map(todo => (
+      <TodoItem key={todo.id} todo={todo} />
+    ))}
+  </ul>
+);
 
 export default TodosList;
