880111e76ac5af639daf6ab0073187fd52d30ecd
Kelvin Bush <kelybush@gmail.com>
2022-05-30 15:02:53 +0000

    remove unused folders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
-import './functionBased/App.css';
-import App from './functionBased/App';
+import './App.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
